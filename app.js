91955bdcf9d1f506d230948dc312db5facbcab5c
Dontha Sumanth <sumanth.dontha@siemens.com>
2023-12-16 18:41:33 +0530

    Replace local with MongoDB database

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const {graphqlHTTP} = require('express-graphql')
 const {buildSchema} = require('graphql')
 const events=[]
 const app = express();
+const Event = require('./models/event')
+const mongoose = require("mongoose");
+require('dotenv').config();
+const UserName = process.env.MONGO_USER;
+const Password = process.env.MONGO_PASSWORD;
+const Mongo_DB = process.env.MONGO_DATABASE;
 app.use(express.json())
 
 app.use('/graphql',graphqlHTTP({
@@ -33,24 +39,36 @@ app.use('/graphql',graphqlHTTP({
         }
     `),
     rootValue: {
-        events: ()=>{
-            return events
+        events: async()=>{
+            const Allevents=await Event.find()
+            console.log(Allevents)
+            return Allevents
         },
-        createEvent:(args)=>{
-            events.splice(0,events.length)
-            const newEvent={
-                _id:Math.random().toString(),
+        createEvent:async(args)=>{
+            const newEvent = new Event({
                 title:args.eventInput.title,
                 description:args.eventInput.description,
                 price:+args.eventInput.price,
-                date:args.eventInput.date
-            }
-            events.push(newEvent)
-            return newEvent
+                date:new Date()
+            });
+            await newEvent.save().then(result=>{
+                console.log(result);
+                return {...result._doc};
+            })
+            .catch(err=>{
+                console.log(err)
+                throw err;
+            })
         }
     },
     graphiql:true
 }));
 
-
-app.listen(3000);
+mongoose.connect(`mongodb+srv://${UserName}:${Password}@eventbooking.powdood.mongodb.net/${Mongo_DB}?retryWrites=true&w=majority`)
+.then(()=>{
+    app.listen(3000);
+    console.log("Connection was Succesfull")
+})
+.catch((err)=>{
+    console.log(err)
+})
